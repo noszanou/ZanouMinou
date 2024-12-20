c9d1869cd585f18e3c6436b8cccaa86c72ef2222
Zanou☻ <noszanou@gmail.com>
2024-12-20 11:58:14 +0100

    :skull:

--- AvaloniaTool/ViewModels/MainViewModel.cs
+++ AvaloniaTool/ViewModels/MainViewModel.cs
@@ -5,14 +5,13 @@ namespace AvaloniaTool.ViewModels;
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
-    private string _greeting;
+    private string? _greeting;
 
-    partial void OnGreetingChanged(string oldValue, string newValue)
+    partial void OnGreetingChanged(string? oldValue, string? newValue)
     {
         if (string.IsNullOrWhiteSpace(newValue))
         {
             Greeting = "Empty";
         }
     }
-
 }

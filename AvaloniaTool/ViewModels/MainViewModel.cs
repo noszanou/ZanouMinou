9fa555cf24929f9b1889a83bdccb396fe6ae3554
Zanou☻ <noszanou@gmail.com>
2024-12-20 11:53:53 +0100

    add palette / style

--- AvaloniaTool/ViewModels/MainViewModel.cs
+++ AvaloniaTool/ViewModels/MainViewModel.cs
@@ -5,5 +5,14 @@ namespace AvaloniaTool.ViewModels;
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
-    private string _greeting = "Kewk";
+    private string _greeting;
+
+    partial void OnGreetingChanged(string oldValue, string newValue)
+    {
+        if (string.IsNullOrWhiteSpace(newValue))
+        {
+            Greeting = "Empty";
+        }
+    }
+
 }
